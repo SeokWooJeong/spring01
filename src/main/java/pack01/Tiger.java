a5daa2fb8ac0eedbb6b406942d7bbece324060b3
SeokWooJeong <ilojloklo@gmail.com>
2019-09-18 10:08:36 +0900

    first

---
+++ src/main/java/pack01/Tiger.java
@@ -0,0 +1,19 @@
+package pack01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Tiger {     
+	@RequestMapping("/t1")    
+	public String func01() {
+		System.out.printf( "함수 콜\n" );
+		return null;    
+	}
+	
+	@RequestMapping("/t2")    
+	public String func02() {
+		System.out.printf( "함수 콜 2 \n" );
+		return "TigerView";    
+	}
+}
